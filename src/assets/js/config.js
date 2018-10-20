99d202e9ae1a7572601b31939a117ed46df3c819
hacca <horiuchi@hacca.cc>
2018-10-20 22:57:19 +0900

    core setting

---
+++ src/assets/js/config.js
@@ -0,0 +1,26 @@
+(function($) {
+ $(function(){
+
+	//スムーズスクロール
+	$('a[href^="#"]').click(function(){
+	 var speed = 600;
+	 var href= $(this).attr("href");
+	 var target = $(href == "#" || href == "" ? 'html' : href);
+	 var position = target.offset().top;
+	 $("html, body").animate({scrollTop:position}, speed, "swing");
+	 return false;
+	});
+	var url = $(location).attr('href');
+	if (url.indexOf("?id=") == -1) {
+	}else{
+	 var url_sp = url.split("?id=");
+	 var hash   = '#' + url_sp[url_sp.length - 1];
+	 var tgt    = $(hash);
+	 var pos    = tgt.offset().top;
+	 $("html, body").animate({scrollTop:pos}, 600, "swing");
+	}
+
+
+ });
+})(jQuery);
+
