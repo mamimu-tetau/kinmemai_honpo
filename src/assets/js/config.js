3d60d8a941198c5b74d54d2be0f61962d706a084
hacca <horiuchi@hacca.cc>
2018-10-31 23:49:09 +0900

    もろもろもろ

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,15 +2,50 @@
     $(function(){
 
 
+        $(".md_header__search input").focus(function(){
+            $(this).parents().children(".icon_nav").addClass("hide");
+        }).blur(function(){
+            $(this).parents().children(".icon_nav").removeClass("hide");
+        });
 
 
-  $(".md_header__search input").focus(function(){
-    $(this).parents().children(".icon_nav").addClass("hide");
-  }).blur(function(){
-    $(this).parents().children(".icon_nav").removeClass("hide");
-  });
-
-
+        //sidrメニュー
+        $('#drawer_trigger').sidr({
+            name: 'drawer',
+            source: '#drawer_menu',
+            side: 'right',
+            renaming: false,
+            displace: false,
+            onOpen: function() {
+                $('#drawer_trigger').addClass('open');
+                $('#drawer_triggerBtn').addClass('active');
+                $('#drawer_cover').fadeIn(300);
+                $('.sidr_slide__close').show();
+                $("meta[name='viewport']").attr('content','width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no');
+            },
+            onClose: function() {
+                $('#drawer_trigger').removeClass('open');
+                $('#drawer_triggerBtn').removeClass('active');
+                $('#drawer_cover').fadeOut(300);
+                $('.sidr_slide__close').hide();
+                $("meta[name='viewport']").attr('content','width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=yes');
+            }
+        });
+        $('#drawer_cover').on("click", function () {
+            $.sidr('close', 'drawer');
+        });
+        $('.sidr_slide__close').on("click", function () {
+            $.sidr('close', 'drawer');
+        });
+        $(window).touchwipe({
+            // wipeRight: function() {
+            // $.sidr('close', 'drawer');
+            // },
+            wipeLeft: function() {
+             $.sidr('open', 'drawer');
+            },
+            preventDefaultEvents: false
+        });
 
     //スムーズスクロール
         $('a[href^="#"]').click(function(){
