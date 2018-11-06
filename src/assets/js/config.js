e0b68213bcbebc06d9f3f6a0c44f07f39feec18a
hacca <horiuchi@hacca.cc>
2018-11-06 10:13:04 +0900

    ガイドぉ

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,6 +1,26 @@
 (function($) {
     $(function(){
 
+        // スムーズスクロール
+        var notList = '#togmenu a[href^="#"], a.remodal-close,a[href^="#0"],a[href^="#wpcf7"]';
+        $('a[href^="#"]').not(notList).click(function(){
+            var speed = 300;
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top - 60;
+            $("html, body").animate({scrollTop:position}, speed, "swing");
+            return false;
+        });
+        var url = $(location).attr('href');
+        if (url.indexOf("?id=") == -1) {
+        }else{
+            var url_sp = url.split("?id=");
+            var hash   = '#' + url_sp[url_sp.length - 1];
+            var tgt    = $(hash);
+            var pos    = tgt.offset().top;
+            $("html, body").animate({scrollTop:pos}, 300, "swing");
+        }
+
 
         $(".md_header__search input").focus(function(){
             $(this).parents().children(".icon_nav").addClass("hide");
@@ -46,25 +66,6 @@
             // },
             preventDefaultEvents: false
         });
-
-    //スムーズスクロール
-        $('a[href^="#"]').click(function(){
-            var speed = 600;
-            var href= $(this).attr("href");
-            var target = $(href == "#" || href == "" ? 'html' : href);
-            var position = target.offset().top;
-            $("html, body").animate({scrollTop:position}, speed, "swing");
-            return false;
-        });
-        var url = $(location).attr('href');
-        if (url.indexOf("?id=") == -1) {
-        }else{
-            var url_sp = url.split("?id=");
-            var hash   = '#' + url_sp[url_sp.length - 1];
-            var tgt    = $(hash);
-            var pos    = tgt.offset().top;
-            $("html, body").animate({scrollTop:pos}, 600, "swing");
-        }
 
 
         //totop
